c1b7490ad9fb5522ef89dd43d48a4cebb9fcf255
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-13 10:38:00 +0200

    add finished new topic validation

--- validate/validateNewTopics.js
+++ validate/validateNewTopics.js
@@ -15,9 +15,21 @@ const validateTopicsTitle = [
     .isLength({ max: 500 })
     .withMessage("Description cannot be longer than 500 chars"),
 
-    body("options")
-     .isArray()
-     .withMessage("Must be an array")
-  ];
+  body("options")
+    .isArray()
+    .withMessage("Must be an array")
+    .isArray({min: 2, max: 5})
+    .withMessage("Options have to be from 2 to 5")
+    .notEmpty()
+    .withMessage("Must not be empty")
+    //Checks for non unique options
+    .custom((tags) => {
+      const unique = new Set(tags);
+      if (unique.size !== tags.length) {
+        throw new Error("Options must be unique");
+      }
+      return true;
+    })
+];
 
-export default validateTopicsTitle
+export default validateTopicsTitle;
