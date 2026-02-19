4e963d0c6395936008a7d9d5a43d550a6557ddfe
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-02-19 09:05:47 +0200

    add CORS

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import topicRoutes from "./router/topicsRouter.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
+// middleware for cors
+
+app.use(
+  cors({
+    origin: `http://localhost:5173`,
+    // credentials: true,
+  }),
+);
+
 // create application/json parser
-const jsonParser = bodyParser.json()
- 
+const jsonParser = bodyParser.json();
 
 app.use("/api/v1/topics", jsonParser, topicRoutes);
 
